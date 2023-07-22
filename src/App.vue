19c649e5cdd706df1cdf865e8f3749863341969b
Rexomon <muhammadindriyo12@gmail.comm>
2023-07-22 17:05:59 +0700

    fixed router and calling it into app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import LoginPage from './views/LoginPage.vue';
+
+
+<script>
+import Login from './components/Login.vue'
+import { defineComponent } from 'vue';
+import Navbar from './components/Navbar.vue'
+import router from './router';
+
+//define 
+export default defineComponent({
+    name: 'appvue',
+    components: {
+        Navbar //call Navbar from import Navbar
+    },
+    router,//call router from import router
+});
 </script>
 
-<template>
 
-  <RouterView />
+<template>
+    <Navbar />
+    <div class="">
+        <router-view></router-view>
+    </div>
 </template>
 
-<style scoped>
 
-</style>
+
+<style></style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,10 +1,17 @@
-<script setup>
+<script>
+import Login from '../components/Login.vue'
+
+export default {
+    name: 'haruslogin',
+    components: { Login }
+}
 </script>
 
 <template>
+    <div class="tempatlogin">
 
+        <Login />
+    </div>
 </template>
 
-<style>
-
-</style>
+<style scoped></style>
